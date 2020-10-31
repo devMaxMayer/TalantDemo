c741c86fc2c550a3105c78842f41ade2f69483a6
devMaxMayer <dev.maxmayer@gmail.com>
2020-11-01 02:30:07 +0400

    events

--- backend/src/main/java/ITksiki/TalantDemo/service/EventService.java
+++ backend/src/main/java/ITksiki/TalantDemo/service/EventService.java
@@ -1,6 +1,20 @@
 package ITksiki.TalantDemo.service;
 
 
+import ITksiki.TalantDemo.entity.Event;
+import ITksiki.TalantDemo.entity.User;
+
+import java.util.List;
+
 public interface EventService {
+    List<Event> findAll();
+
+    Event findById(Long id);
+
+    Event save (Event event);
+
+    void deleteById(Long id);
+
+    Event ChangeSubscription (Event event, User user);
 
 }
